2caa8d4df525a3ccbe0cae0e83e04ea54053472d
regyna43 <regynakenzo@gmail.com>
2024-09-07 11:02:21 +0700

    Add files via upload

---
+++ pelatihan coding/day1.py
@@ -0,0 +1,37 @@
+# a = "apel" #"a" adalah variabel dan "apel" adalah data
+# print(a)
+
+# buah = "jeruk" #ini adalah tipe data 'string'
+# harga = 15000 #ini adalah tipedata 'integer'
+# berat = 1.5 #ini adalah tipedata 'float'
+# beli = True #ini adalah tipedata 'boolean'
+
+# print(buah)
+# print(harga)
+# print(berat)
+# print(beli)
+
+# format string
+# nama = 'siti'
+# umur = 16
+# alamat = 'jl buntu'
+# tinggi = 1.75
+
+# print (f"nama saya {nama}.saya berusia {umur} tahun,dan tinggi saya {tinggi} meter.Saya tinggal di {alamat}.")
+
+# input 
+nama = input ("siapa nama kamu?")
+print (f"Halo, {nama}")
+
+umur = input (input("umur kamu berapa?"))
+print(f"{umur} tahun")
+
+print("Halo, perkenalkan nama aku Bot!  ")
+print("Halo, Bot!")
+nama = input ("nama,kamu siapa?")
+print(fnamaku {nama})
+print(f"Halo, {nama. Salam kenal!}")
+print (f"Kamu sibuk apa sekarang, {nama}")
+kegiatan = input ("kesibukanmu?")
+print (f"aku lagi sibuk {kegiatan} sekarang.")
+print (f"oke, semoga {kegiatan} nyaman.")
